9fc87bfd6d0fa3b7287f7eb3223a5a61d362be76
Dias <dias@littlebit.sg>
2021-07-22 17:29:31 +0800

    Added getters for oracle, fee and job id

--- contracts/LockedCacheGoldAPIConsumer.sol
+++ contracts/LockedCacheGoldAPIConsumer.sol
@@ -55,6 +55,30 @@ contract LockedCacheGoldAPIConsumer is ChainlinkClient, Ownable {
     }
 
     /**
+     * Get the oracle
+     * @return oracleAddress 
+     */ 
+    function getOracle() external view returns(address oracleAddress) {
+        return oracle;
+    }
+
+    /**
+     * Get the feeAmount that is in LINK tokens
+     * @return feeAmount
+     */ 
+    function getFees() external view returns(uint256 feeAmount) {
+        return fee;
+    }
+
+    /**
+     * Get the jobId
+     * @return jobIdentifier
+     */ 
+    function getJobId() external view returns(bytes32 jobIdentifier) {
+        return jobId;
+    }
+
+    /**
      * Receive the response in the form of requestId
      * @dev Kept pathOfValue as dynamic in case the path changes in future or we want to accomodate more in the same link
      * @param _pathOfValue This is the path of the value in the json object response in the url
